760586329251dc92b7a811bef55317341b08cfbc
acomarcho <acoxstpd@gmail.com>
2022-11-22 10:32:36 +0700

    Feat: Set up login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,69 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./Login.module.css";
+import Logo from "../../assets/logo-dark.svg";
+
+import AuthWrapper from "../../components/AuthWrapper/AuthWrapper";
+import FormGroup from "../../components/AuthForm/FormGroup";
+import FormButton from "../../components/AuthForm/FormButton";
+
 const Login = () => {
-    return <h1>Login page!</h1>; 
-}
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [isUsernameValid, setIsUsernameValid] = useState<boolean>(false);
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsUsernameValid(username.length > 0);
+  }, [username]);
+
+  useEffect(() => {
+    setIsPasswordValid(password.length > 0);
+  }, [password]);
+
+  const onLogin = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    // TODO: Send POST request to REST API
+    alert("Login button clicked!");
+  };
+
+  return (
+    <AuthWrapper>
+      <>
+        <header className={styles.header}>
+          <img src={Logo} alt="Binotify Logo" />
+          <p>Log in to start using Binotify Premium!</p>
+        </header>
+        <form className={styles.form}>
+          <FormGroup
+            id="username"
+            type="text"
+            label="Enter your username:"
+            placeholder="johndoe"
+            value={[username, setUsername]}
+            status={isUsernameValid}
+            errorText="Username cannot be empty!"
+          />
+          <FormGroup
+            id="password"
+            type="password"
+            label="Enter your password:"
+            placeholder=""
+            value={[password, setPassword]}
+            status={isPasswordValid}
+            errorText="Password cannot be empty!"
+          />
+          <FormButton
+            text="Register"
+            onClickFunction={onLogin}
+            disabled={!isUsernameValid || !isPasswordValid}
+          />
+        </form>
+      </>
+    </AuthWrapper>
+  );
+};
 
 export default Login;
