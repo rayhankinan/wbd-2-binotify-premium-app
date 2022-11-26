d2862687f8babc7ec86651223fdad2539fd5f079
acomarcho <acoxstpd@gmail.com>
2022-11-26 12:49:48 +0700

    Feat: Basic registration wiring with REST API

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -10,17 +10,28 @@ import FormButton from "../../components/AuthForm/FormButton";
 import { validateUsername } from "../../utils/validateUsername";
 import { validateEmail } from "../../utils/validateEmail";
 
+import { REST_BASE_URL } from "../../constants/constants";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Register = () => {
+  const [name, setName] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmation, setConfirmation] = useState<string>("");
 
+  const [isNameValid, setIsNameValid] = useState<boolean>(false);
   const [isUsernameValid, setIsUsernameValid] = useState<boolean>(false);
   const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
   const [isPasswordValid, setIsPasswordValid] = useState<boolean>(false);
   const [isConfirmationValid, setIsConfirmationValid] =
     useState<boolean>(false);
+
+  useEffect(() => {
+    setIsNameValid(name.length > 0);
+  }, [username]);
 
   useEffect(() => {
     setIsUsernameValid(validateUsername(username));
@@ -35,21 +46,63 @@ const Register = () => {
     setIsConfirmationValid(password === confirmation);
   }, [password, confirmation]);
 
-  const onRegister = (e: React.SyntheticEvent) => {
+  const onRegister = async (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    // TODO: Send POST request to REST API
-    alert("Register button clicked!");
+    // Build JSON data to be sent
+    const requestBody = {
+      name,
+      username,
+      email,
+      password,
+    };
+
+    // Send POST request
+    const response = await fetch(`${REST_BASE_URL}/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+    
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.log(data.message);
+      toast.error(data.message, {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    } else {
+      // TODO: Save authorization bearer in local storage and login user
+    }
   };
 
   return (
     <AuthWrapper>
       <>
+        <ToastContainer />
         <header className={styles.header}>
           <img src={Logo} alt="Binotify Logo" />
           <p>Sign up for free to start using Binotify Premium!</p>
         </header>
         <form className={styles.form}>
+          <FormGroup
+            id="name"
+            type="text"
+            label="Enter your name:"
+            placeholder="johndoe"
+            value={[name, setName]}
+            status={isNameValid}
+            errorText="Name cannot be empty!"
+          />
           <FormGroup
             id="username"
             type="text"
@@ -86,10 +139,19 @@ const Register = () => {
             status={isConfirmationValid}
             errorText="Confirmation password is different than password!"
           />
-          <FormButton text="Register" onClickFunction={onRegister} disabled={!isUsernameValid || !isConfirmationValid || !isPasswordValid || !isConfirmationValid}/>
+          <FormButton
+            text="Register"
+            onClickFunction={onRegister}
+            disabled={
+              !isUsernameValid ||
+              !isConfirmationValid ||
+              !isPasswordValid ||
+              !isConfirmationValid
+            }
+          />
         </form>
       </>
-    </AuthWrapper> 
+    </AuthWrapper>
   );
 };
 
